d492bed70c8094a5a89640ee0de622b28c1c3117
TomasCM <olivo_5@hotmail.com>
2015-01-21 00:31:23 +0100

    Assignment2
    Quiz week 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y){
+                first <<- y
+                inv <<- NULL
+        }
+        get <-function() first
+        setinv <- function(x) inv<<-(solve(x))
+        getinv<- function() inv
+        list(set = set, get = get,
+                setinv = setinv,  getinv = getinv)
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<- function(x, ...){
+        inv <-x$getinv()
+        if (!is.null(inv)) {
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinv(inv)
+        inv
+        
 }
+## Examples
+m1 <- makeCacheMatrix() # generates functions
+a <- matrix(c(7,5,0,4),2,2)
+m1$set(a) # load matrix
+b <- cacheSolve(m1) # Read matrix inverse a
+a %*% b # test
